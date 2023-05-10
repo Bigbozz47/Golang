608467af9d90e3a2504d1b46a927d79013cd49fb
Delano Yusuf Habibie <delanoyusufhabibie47@gmail.com>
2023-05-10 21:04:45 +0700

    first commit

---
+++ variabel.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "Delano Yusuf"
+	var lastName string
+
+	lastName = " Habibie"
+
+	fmt.Print("Halo ", firstName, lastName, "!\n")
+	fmt.Print("Hello world\n")
+}
