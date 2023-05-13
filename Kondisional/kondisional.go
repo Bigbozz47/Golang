03d1ee5e7d2eeae715a10437bb89d1ab2b979930
Delano Yusuf Habibie <delanoyusufhabibie47@gmail.com>
2023-05-14 04:10:30 +0700

    Revisi 1 kondisional

--- Kondisional/kondisional.go
+++ Kondisional/kondisional.go
@@ -3,14 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var point = 10
-	if point == 10 && point < 20 {
-		fmt.Println("lulus dengan nilai sempurna")
-	} else if point > 5 {
-		fmt.Println("lulus")
-	} else if point == 4 {
-		fmt.Println("hampir lulus")
+	var point = 8840.0
+	if percent := point / 100; percent >= 100 {
+		fmt.Printf("%.1f%s perfect!\n", percent, "%")
+	} else if percent >= 70 {
+		fmt.Printf("%.1f%s good!\n", percent, "%")
 	} else {
-		fmt.Printf("tidak lulus. nilai anda %d\n", point)
+		fmt.Printf("%.1f%s not bad!\n", percent, "%")
 	}
 }
